092877e5c513d5a8a6dada7270e61a0f01dae727
Lavkush <122461964+Lavkush1809@users.noreply.github.com>
2023-07-09 12:44:09 +0530

    Add files via upload

---
+++ rodcutting.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int rodCutting(vector<int> &prices){
+    int L= prices.size();
+    vector<int> dp(L+1,0);
+    for(int l=1; l<=L;l++){
+        dp[l]=prices[l-1];
+        for(int j=1; j<=l; j++){
+            dp[l]= max(dp[l], dp[j]+ dp[l-j]);
+        }
+    }
+    return dp[L-1];
+}
+
+ 
+
+
+int main(){
+
+}
